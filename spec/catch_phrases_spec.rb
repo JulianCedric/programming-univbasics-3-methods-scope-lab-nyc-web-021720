2627b9989d3b7b09f87add3489873d6bd8d98028
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-02-06 22:32:08 +0000

    Automatically backed up by Learn

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -24,3 +24,17 @@ describe "any_phrase" do
     expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
   end
 end
+
+
+
+
+
+
+
+
+# describe "any_phrase" do
+#   it "takes in an argument and puts out the catch phrase" do
+#     phrase = "Do A Barrel Roll!"
+#     expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
+#   end
+# end
